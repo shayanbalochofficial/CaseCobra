dd5d108754d24f332998cb7b2b43d90f92508311
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-11-10 01:04:00 +0500

    Creating our configurator page
    Time Tracked: 30 hours

---
+++ src/src/validators/option-validator.ts
@@ -0,0 +1,85 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
+
+import { PRODUCT_PRICES } from "@/config/products";
+
+export const COLORS = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  { label: "Blue", value: "blue", tw: "blue-950" },
+  { label: "Rose", value: "rose", tw: "rose-950" },
+] as const;
+
+export const MODELS = {
+  name: "models",
+  options: [
+    {
+      label: "iPhone X",
+      value: "iphonex",
+    },
+    {
+      label: "iPhone 11",
+      value: "iphone11",
+    },
+    {
+      label: "iPhone 12",
+      value: "iphone12",
+    },
+    {
+      label: "iPhone 13",
+      value: "iphone13",
+    },
+    {
+      label: "iPhone 14",
+      value: "iphone14",
+    },
+    {
+      label: "iPhone 15",
+      value: "iphone15",
+    },
+    {
+      label: "iPhone 16",
+      value: "iphone16",
+    },
+    {
+      label: "iPhone 17",
+      value: "iphone17",
+    },
+  ],
+} as const;
+
+export const MATERIALS = {
+  name: "material",
+  options: [
+    {
+      label: "Silicone",
+      value: "silicone",
+      description: undefined,
+      price: PRODUCT_PRICES.material.silicone,
+    },
+    {
+      label: "Soft PolyCarbonate",
+      value: "polycarbonate",
+      description: "Scatch-resitant coating",
+      price: PRODUCT_PRICES.material.polycarbonate,
+    },
+  ],
+};
+
+export const FINISHES = {
+  name: "Finishes",
+  options: [
+    {
+      label: "Smooth Finish",
+      value: "smooth",
+      description: undefined,
+      price: PRODUCT_PRICES.finish.smooth,
+    },
+    {
+      label: "Textured Finish",
+      value: "textured",
+      description: "Soft grippy texture",
+      price: PRODUCT_PRICES.finish.textured,
+    },
+  ],
+};
